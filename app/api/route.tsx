35904b3091038742cb92b6756dd69f77b7c21a15
Ramananda Panda <ramanandapanda@hotmail.com>
2024-04-21 13:12:40 +0530

    interate open ai

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,11 +1,33 @@
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
+async function getfunFactsFromOpenAI(topic: string) {
+  const openai = new OpenAI({
+    apiKey: "",
+  });
+  const res = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You will be provided with a topic and your task will be to generate 5 fun facts as fun_facts of around 10 words",
+      },
+      {
+        role: "user",
+        content: `Topic ${topic}`,
+      },
+    ],
+  });
+  return res.choices[0].message.content;
+}
 
+export async function POST(req: Request) {
+  const resBody = await req.json();
 
+  const res = await getfunFactsFromOpenAI(resBody["topic"] ?? "");
 
-export async function POST() {
-
-    return NextResponse.json({
-        abc: "Some Result",
-    })
+  return NextResponse.json({
+    result: res,
+  });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,64 @@
-'use client'
+"use client";
+import Error from "next/error";
 import React, { FormEvent } from "react";
 
 export default function Home(): JSX.Element {
-  const [topicText, setTopicText] = React.useState('');
+  const [topicText, setTopicText] = React.useState("");
   const [isLoading, setIsLoading] = React.useState(false);
-  const [result, setResult] = React.useState('');
-  const [err, setError] = React.useState('');
-  function submitForm(e: FormEvent) {
+  const [result, setResult] = React.useState<string[]>([]);
+  const [err, setError] = React.useState("");
+  async function submitForm(e: FormEvent) {
     e.preventDefault();
     setIsLoading(true);
-    fetch("/api", {
-      method: "POST",
-      body: JSON.stringify({
-        topic: topicText
-      })
-    }).then((res) => {
+    try {
+      const res = await fetch("/api", {
+        method: "POST",
+        body: JSON.stringify({
+          topic: topicText,
+        }),
+      });
       setIsLoading(false);
-      return res.json()
-    }).then((response) => {
-      // setIsLoading(false)
-      setResult(response.abc)
-    }).catch((err) => {
-      setError(err.toStrring())
-    })
-
+      const response = await res.json();
+      setResult((response.result as string).split("\n"));
+    } catch (err) {
+      // setError(err)
+    }
   }
   return (
     <main className="flex min-h-screen flex-col items-start justify-center p-24">
       <h1 className="text-2xl mb-8">Fun fact of your choice</h1>
-      {
-        isLoading ?
-          <span>Loading...</span>
-          : <>
-            <form onSubmit={(e) => submitForm(e)}>
-              <input type="text" value={topicText} onChange={(e) => setTopicText(e.target.value)} className="p-2 mr-1" placeholder="Enter A Topic" />
-              <button type="submit" className="p-2 mr-2 bg-sky-500 rounded">Submit</button>
-              <button type="reset" className="p-2 bg-green-500 rounded">Reset</button>
-            </form>
-            <div>
-              {
-                err != '' ?
-                  <span className="text-red-500" >{err}</span>
-                  :
-                  <span className="text-green-500">{result}</span>
-              }
-            </div>
-          </>
-      }
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <>
+          <form onSubmit={(e) => submitForm(e)}>
+            <input
+              type="text"
+              value={topicText}
+              onChange={(e) => setTopicText(e.target.value)}
+              className="p-2 mr-1"
+              placeholder="Enter A Topic"
+            />
+            <button type="submit" className="p-2 mr-2 bg-sky-500 rounded">
+              Submit
+            </button>
+            <button type="reset" className="p-2 bg-green-500 rounded">
+              Reset
+            </button>
+          </form>
+          <div>
+            {err != "" ? (
+              <span className="text-red-500">{err}</span>
+            ) : (
+              <ul>
+                {result.map((ef) => (
+                  <li>{ef}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </>
+      )}
     </main>
   );
 }
